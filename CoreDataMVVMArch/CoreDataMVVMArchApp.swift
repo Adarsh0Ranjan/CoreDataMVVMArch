91ffe788cf619924a30d3e00597103307bede8ee
Adarsh Ranjan <adarsh.ranjan@roro.io>
2024-12-29 12:42:49 +0000

    Initial Commit

---
+++ CoreDataMVVMArch/CoreDataMVVMArchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataMVVMArchApp.swift
+//  CoreDataMVVMArch
+//
+//  Created by Adarsh Ranjan on 29/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataMVVMArchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
